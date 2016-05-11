add57c0d2e9b38cc1ce82edd99f17b855ce8ca0c
Ryan Goodfellow <rgoodfel@isi.edu>
2016-05-11 11:36:28 -0700

    build seems to work

---
+++ deps/all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+./ubuntu.sh
+./boost.sh
+./double-conversion.sh
+./gflags.sh
+./glog.sh
+./folly.sh
+./wangle.sh
+./proxygen.sh
+
---
+++ deps/ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+  uuid-dev \
+  libpq-dev
+#  libldap-dev \
+#  libgss-dev \
+#  libsasl2-dev \
+#  libcrypt20-dev \
+#  libdb-dev \
+#  libsqlite3-dev \
+#  libmysqlclient-dev \
+#  libhogweed4 \
+#  libp11-dev
